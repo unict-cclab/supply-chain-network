3f067b32fce62dea83c8cd24759e2c2df0a25da2
angelo <marchese.angelo1@gmail.com>
2020-09-28 13:11:35 +0200

    Modify home page in frontend application

--- dockerfiles/frontend/src/components/Home.js
+++ dockerfiles/frontend/src/components/Home.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
+import logo from '../logo.svg';
 
 export default class Home extends Component {
 
   render() {
     return (
-      <div></div>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p className="mt-5">
+            A traceability system for agri-food supply chain management
+          </p>
+        </header>
+      </div>
     );
   }
 }
